2dacee58ced231496decbc8b3216c3ee2850416e
trungdoanhumg <2021050195@student.humg.edu.vn>
2024-06-17 12:38:39 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NTD_Baithi2324.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
